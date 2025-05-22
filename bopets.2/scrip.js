ca7d8af9e4b48d1275a3157927dd7e5a899d1782
nataly26m <natalyvelazquezz072@gmail.com>
2025-05-22 09:50:44 -0700

    Add files via upload

---
+++ bopets.2/scrip.js
@@ -0,0 +1,8 @@
+<script>
+  const toggle = document.getElementById('menu-toggle');
+  const navLinks = document.getElementById('nav-links');
+
+  toggle.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+  });
+</script>
