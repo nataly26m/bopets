4d39afeef5cada699491af3efb969a34c4261afc
nataly26m <natalyvelazquezz072@gmail.com>
2025-05-22 12:03:08 -0700

    Delete bopets.2 directory

--- bopets.2/scrip.js
+++
@@ -1,8 +0,0 @@
-<script>
-  const toggle = document.getElementById('menu-toggle');
-  const navLinks = document.getElementById('nav-links');
-
-  toggle.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
-  });
-</script>
